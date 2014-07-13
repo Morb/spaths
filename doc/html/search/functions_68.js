ab5c52136bb6f6f2f7d42a27523b0c795732dc25
Federico Terzi <terzi.federico@gmail.com>
2014-07-13 12:56:02 +0200

    Rigenerata documentazione

--- doc/html/search/functions_68.js
+++ doc/html/search/functions_68.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['handler_5fdelete_5fevent',['handler_delete_event',['../SPaths_8cc.html#af41b4bf0ba2f0884b433dc12b71c1947',1,'SPaths.cc']]],
+  ['handler_5fdialog_5fdelete_5fevent',['handler_dialog_delete_event',['../SPaths_8cc.html#a7760896a079d362315a13adbd4cb8557',1,'SPaths.cc']]],
   ['help_5fhandler',['help_handler',['../SPaths_8cc.html#a89b16bd893fb653d417b805484c330b4',1,'SPaths.cc']]],
   ['hide_5fmain_5fwindow',['hide_main_window',['../SPaths_8cc.html#ad3754aea40d219de8dc0c6c3be9c00dd',1,'SPaths.cc']]]
 ];
